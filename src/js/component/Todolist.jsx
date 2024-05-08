7e246bac2b3d4ccea05ad3ccd263c518cba5e087
Estrella Benítez Buitrago <156102986+Llitabb@users.noreply.github.com>
2024-05-08 16:19:24 +0000

    First commit

---
+++ src/js/component/Todolist.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+
+const Todolist = () => {
+
+    
+    return (
+        <div>
+            <h1> To Do List</h1>
+        </div>
+    )
+};
+
+
+
+export default Todolist;
