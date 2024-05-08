ac2ce56e6bc831f27df0a0f32b74d9cf25a03d9c
Estrella Benítez Buitrago <156102986+Llitabb@users.noreply.github.com>
2024-05-08 17:46:34 +0000

    second commit

--- src/js/component/Todolist.jsx
+++ src/js/component/Todolist.jsx
@@ -2,10 +2,18 @@ import React, {useState} from "react";
 
 const Todolist = () => {
 
-    
+
     return (
-        <div>
+        <div className="container">
             <h1> To Do List</h1>
+            <ul>
+                <li><input type="text" placeholder="What do you need to remember to do"></input>
+                </li>
+                <li>Make the bed <i class="fa-solid fa-x"></i></li>
+            </ul>
+            <div>
+            1 tarea
+            </div>
         </div>
     )
 };
