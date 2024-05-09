5def4c3570ddfb0ea8e4712ff63235bfc8be5816
Estrella Benítez Buitrago <156102986+Llitabb@users.noreply.github.com>
2024-05-09 20:43:02 +0000

    add some functions

--- src/js/component/Todolist.jsx
+++ src/js/component/Todolist.jsx
@@ -2,14 +2,32 @@ import React, {useState} from "react";
 
 const Todolist = () => {
 
+    const [ inputValue, setInputValue ] = useState("")
+    const [ todos, setTodos ] = useState([])
 
     return (
         <div className="container">
-            <h1> To Do List</h1>
+            <h1> To Do List </h1>
             <ul>
-                <li><input type="text" placeholder="What do you need to remember to do"></input>
+                <li>
+                    <input 
+                    type="text"
+                    onChange={(e) => setInputValue(e.target.value)}
+                    value={inputValue}
+                    onKey={(e) => {
+                        if (e.key === "Enter") {
+                            setTodos(todos.concat(inputValue));
+                            setInputValue("");
+                        }
+                    }}
+                    placeholder="What do you need to remember to do?"/>
                 </li>
-                <li>Make the bed <i class="fa-solid fa-x"></i></li>
+                {todos.map ((t) => (
+                <li>
+                    {t} <i class="bi bi-trash3-fill"></i>
+                </li>
+                ))}
+                <li>Make a cake</li>
             </ul>
             <div>
             1 tarea
